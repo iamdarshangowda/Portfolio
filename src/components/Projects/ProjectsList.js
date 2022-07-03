07f3ca57ed5d18a69283169741cacf011aa4f7a2
iamdarshangowda <iamdarshangowda@gmail.com>
2022-07-03 20:53:25 +0530

    Projects List Added

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -3,29 +3,43 @@ import projects from "./Data";
 
 export default function ProjectList() {
   return (
-    <div className="projects-container">
-      {projects.map((item) => {
-        return (
-          <div className="project">
-            <img
-              src={item.image}
-              style={{ width: "300px" }}
-              alt="project demo"
-            />
-            <h1>{item.name}</h1>
-            <p>{item.about}</p>
-            <p>
-              <span>Skills:</span>
-              {item.skills}
-            </p>
-            <p>
-              <span>Tools:</span>
-              {item.tools}
-            </p>
-            <a href={item.url}>LIVE DEMO</a>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="projects-container">
+        {projects.map((item) => {
+          return (
+            <div className="project">
+              <img
+                src={item.image}
+                style={{ width: "300px", height: "170px", borderRadius: "8px" }}
+                alt="project demo"
+              />
+              <h1>{item.name}</h1>
+              <div className="about-project">
+                <p>{item.about}</p>
+                <p>
+                  <span className="tag">Skills: </span>
+                  {item.skills}
+                </p>
+                <p>
+                  <span className="tag">Tools: </span>
+                  {item.tools}
+                </p>
+              </div>
+              <a href={item.url} target="_blank">
+                <button className="demo-btn">LIVE PAGE</button>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <div className="git">
+        <a
+          href="https://github.com/iamdarshangowda?tab=repositories"
+          target="_blank"
+        >
+          <button className="demo-btn more-btn">More Projects</button>
+        </a>
+      </div>
+    </>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function Skills() {
   return (
     <div>
-      <h1>This is Skills</h1>
+      <h1>Developer Skills:</h1>
+      <ul className="list">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>BootStrap</li>
+        <li>Sassy SCSS</li>
+        <li>JavaScript</li>
+        <li>GIT and GitHub</li>
+        <li>Responsive Web Development</li>
+        <li>ReactJs</li>
+      </ul>
     </div>
   );
 }
