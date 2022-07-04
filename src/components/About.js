1bc610ba8623c76edb3194f3bb25dd4fff78dfaf
iamdarshangowda <iamdarshangowda@gmail.com>
2022-07-04 20:35:57 +0530

    skills added

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <img
+        src={`${process.env.PUBLIC_URL}/images/profile-pic.jpg`}
+        alt="darshan profile photo"
+        style={{ width: "300px", borderRadius: "18px" }}
+      />
+      <div>
+        <h1>Nice to meet You!</h1>
+        <p>
+          I'm a Front End Web Developer with a passion for creating neat,
+          responsive websites.
+        </p>
+        <p>
+          A self-taught aspiring Front-End Web Developer with high vision and
+          forward thinking. Committed to learning and self-development to
+          achieve better results. Proficient with HTML, CSS and JavaScript.
+          Always open to constructive feedback. Effective Problem-Solving skills
+          with ability to work as an individual or in a team environment.
+        </p>
+        <p>Can't wait to work with you!</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,56 @@
 import React from "react";
+import { FaFreeCodeCamp } from "react-icons/fa";
+import { TbPentagon } from "react-icons/tb";
+import { GrCertificate } from "react-icons/gr";
 
 export default function Skills() {
   return (
-    <div>
+    <div class="skill-container">
       <h1>Developer Skills:</h1>
       <ul className="list">
         <li>HTML</li>
         <li>CSS</li>
         <li>BootStrap</li>
-        <li>Sassy SCSS</li>
         <li>JavaScript</li>
         <li>GIT and GitHub</li>
         <li>Responsive Web Development</li>
         <li>ReactJs</li>
       </ul>
+      <div className="certify">
+        <h1>Certification</h1>
+        <div className="certify-links">
+          <a
+            href="https://www.freecodecamp.org/certification/iamdarshangowda/responsive-web-design"
+            target="_blank"
+          >
+            <span className="web">
+              <FaFreeCodeCamp />
+              FreeCodeCamp:{" "}
+            </span>
+            Responsive Web Design (HTML and CSS)
+          </a>
+          <a
+            href="https://www.freecodecamp.org/certification/iamdarshangowda/javascript-algorithms-and-data-structures"
+            target="_blank"
+          >
+            <span className="web">
+              <FaFreeCodeCamp />
+              FreeCodeCamp:{" "}
+            </span>
+            JavaScript Data Structures and Algorithms
+          </a>
+          <a
+            href="https://triplebyte.com/tb/darshan-gowda-ezclzmz/certificate"
+            target="_blank"
+          >
+            <span className="web">
+              <TbPentagon />
+              TripleByte:{" "}
+            </span>
+            Frontend and ReactJs
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
